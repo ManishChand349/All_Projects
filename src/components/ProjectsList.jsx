9482237037c711a89505fd8bba5ff58f06a30d10
Manish Chand <manishchandr349@gmail.com>
2023-12-01 13:24:37 +0530

    Adding

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -25,7 +25,23 @@ const ProjectsList = () => {
    
     // Add more blog entries as needed
   ];
+  const MyComponents = [
+    {
+      id: 1,
+      title: "College Site",
+      content:"Website involves creating a platform",
+      git: "https://github.com/ManishChand349/CollegeSite",
+      url: "https://college-site-one.vercel.app/",
+    },
+    
+    
+   
+    // Add more blog entries as needed
+  ];
   return (
+    <>
+
+   
     <div className="bg-gray-100 p-8">
       {/* Recent Blogs Section */}
       <div className="mb-8 mx-auto max-w-7xl ">
@@ -51,6 +67,32 @@ const ProjectsList = () => {
         </div>
       </div>
     </div>
+    <div className="bg-gray-100 p-8">
+      {/* Recent Blogs Section */}
+      <div className="mb-8 mx-auto max-w-7xl ">
+        <h2 className="text-5xl font-semibold mb-4  text-center">React Components</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {MyComponents.map((data) => (
+            <div
+              key={data.id}
+              className="bg-red-50 p-4 rounded shadow-md bg-gradient-to-r from-red-400 to-blue-500 hover:from-pink-500 hover:to-white-500 "
+            >
+              <h3 className=" font-semibold  mb-2 text-2xl ">{data.title}</h3>
+              <p className="text-gray-700">{data.content}</p>
+              <div className="flex justify-between mt-2">
+                <a href={data.git} target="_blank">
+                  <AiOutlineGithub className="text-3xl cursor-pointer" />
+                </a>
+                <a href={data.url} target="_blank">
+                  <FaArrowUpRightFromSquare className="text-3xl cursor-pointer" />
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+    </>
   );
 };
 
